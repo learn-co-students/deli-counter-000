4564d8e489683e6ebe499362d8e64c07d1ef6f3c
gjc2118 <gjc2118@gmail.com>
2015-05-19 23:41:09 -0700

    Nid:

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+
+katz_deli = []
+
+def take_a_number(line, name)
+  current_number = line.length+1
+  puts "Welcome, #{name}. You are number #{current_number} in line."
+  line << "#{name}"
+  katz_deli = line
+end
+
+
+def now_serving(line)
+  if line.length ==0
+    puts "There is nobody waiting to be served!"
+  else
+    name = line.shift
+    puts "Currently serving #{name}."
+    katz_deli = line
+  end
+end
+
+def line(line)
+  if line.length ==0
+    puts "The line is currently empty."
+  else
+    response = "The line is currently:"
+    counter=1
+    line.each do |person|
+      response << " #{counter}. "
+      response << "#{person}"
+      counter+=1
+    end
+    puts response
+  end
+end
